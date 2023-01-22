c59e0bb5562161dae6687d10fc11edb11a7235b8
frxnpt <62038055+frxnpt@users.noreply.github.com>
2023-01-22 23:25:30 +0100

    feat: create reducer to update and delete clients

--- src/features/dashboard/clients/clientSlice.ts
+++ src/features/dashboard/clients/clientSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-import type { PayloadAction } from '@reduxjs/toolkit'
+import type { PayloadAction } from "@reduxjs/toolkit";
 import { Client } from "./client";
 
 export interface ClientState {
@@ -10,15 +10,30 @@ export interface ClientState {
 const initialState: ClientState = {
 	value: [
 		{
+			id: "1",
 			name: "fran",
+			surname: "perea",
+			phone: "3452345234",
+			email: "fran@fran.fran",
+			summary: "hot",
 			show: true,
 		},
 		{
-			name: "stijn",
-			show: false,
+			id: "2",
+			name: "pedro",
+			surname: "perea",
+			phone: "3452345234",
+			email: "fran@fran.fran",
+			summary: "hot",
+			show: true,
 		},
 		{
-			name: "gabriel",
+			id: "3",
+			name: "juan",
+			surname: "perea",
+			phone: "3452345234",
+			email: "fran@fran.fran",
+			summary: "hot",
 			show: true,
 		},
 	],
@@ -29,10 +44,29 @@ export const clientSlice = createSlice({
 	initialState,
 	reducers: {
 		createClient: (state, action: PayloadAction<Client>) => {
-			state.value.push(action.payload)
-		}
+			state.value.push(action.payload);
+		},
+		updateClientById: (state, action) => {
+			const { id, name, surname, email, phone, summary } = action.payload;
+			const foundClient = state.value.find((client) => client.id === id);
+			if (foundClient) {
+				foundClient.name = name;
+				foundClient.surname = surname;
+				foundClient.email = email;
+				foundClient.phone = phone;
+				foundClient.summary = summary;
+			}
+		},
+		deleteClient: (state, action) => {
+			const foundClient = state.value.find(
+				(client) => client.id === action.payload
+			);
+			if (foundClient) {
+				state.value.splice(state.value.indexOf(foundClient), 1);
+			}
+		},
 	},
 });
 
-export const {createClient} = clientSlice.actions
+export const { createClient, updateClientById } = clientSlice.actions;
 export default clientSlice.reducer;
