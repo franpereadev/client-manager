d556d770697878f18ef914b9741734eff1016223
frxnpt <62038055+frxnpt@users.noreply.github.com>
2023-01-22 23:27:07 +0100

    feat: add breadcrumb to client page

--- src/features/dashboard/clients/components/Clients.tsx
+++ src/features/dashboard/clients/components/Clients.tsx
@@ -1,15 +1,19 @@
 import { useAppDispatch, useAppSelector } from "../../../../app/hooks";
+import Breadcrumb from "../../../shared/components/Breadcrumb/Breadcrumb";
 import ClientForm from "./ClientForm";
 import ClientTable from "./ClientTable";
 
 function Clients() {
-	  // The `state` arg is correctly typed as `RootState` already
-	  const count = useAppSelector((state) => state.clients.value)
-	  const dispatch = useAppDispatch()
+	// The `state` arg is correctly typed as `RootState` already
+	const count = useAppSelector((state) => state.clients.value);
+	const dispatch = useAppDispatch();
 	return (
 		<>
-			<ClientTable />
-			<ClientForm />
+			<div className="rounded bg-white m-5 px-10 py-10">
+				<Breadcrumb />
+				<ClientTable />
+				<ClientForm />
+			</div>
 		</>
 	);
 }
