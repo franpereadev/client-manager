a20fdc493546620dec3fb3a7eb5cff1390041bf9
frxnpt <62038055+frxnpt@users.noreply.github.com>
2023-01-22 23:27:29 +0100

    fix: correct layout for dashboard pages

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -1,16 +1,13 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
 import SideBar from "../shared/components/SideBar";
 import sideBarLinks from "../shared/components/SideBar/sideBarLinks";
 
-
 function Dashboard() {
 	return (
 		<>
-			<div className="flex">
+			<div className="flex bg-main-body font-[Poppins] min-h-screen">
 				<SideBar links={sideBarLinks} />
-				DASH
-				<div style={{ backgroundColor: "red" }}>
+				<div className="flex-1 min-w-0 overflow-auto">
 					<Outlet />
 				</div>
 			</div>
