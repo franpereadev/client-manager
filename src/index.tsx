1432a97bc7038dc3c3fd85c4e3d1e0b97c655bd8
frxnpt <62038055+frxnpt@users.noreply.github.com>
2023-01-22 23:23:58 +0100

    chore: update dependencies for the project update logos and setup tailwind styles

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,17 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-
+import { far } from "@fortawesome/free-regular-svg-icons";
+import {
+	faB,
+	faChevronRight,
+	faDumbbell,
+	faUserGroup,
+	faHouse,
+	faS,
+} from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+library.add(faB, faS, far, faHouse, faUserGroup, faDumbbell, faChevronRight);
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,tsx}"],
+  content: ["./src/**/*.{html,js,tsx}",
+    "./src/features/**/*.{html,js,tsx}",
+    "./src/features/dashboard/**/*.{html,js,tsx}",
+    "./src/features/dashboard/clients/*.{html,js,tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        "sidebar": {
+          900: '#2F2B54',
+          800: '#3F3B66',
+          700: '#4F4B78',
+          600: '#5F5A89',
+          500: '#6F6A9B',
+          400: '#7F7AAD',
+          300: '#8F8ABF',
+          200: '#9F99D0',
+          100: '#AFA9E2',
+        },
+        "sidebar-text": '#F5F5F7',
+        "primary-button": "#545FFF",
+        "main-body": "#C7CACE"
+      },
+    },
   },
   plugins: [],
 }
