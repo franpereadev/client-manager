4aec8b618d1fc3f39e2f27a733f7687f95f0577c
frxnpt <62038055+frxnpt@users.noreply.github.com>
2023-01-22 23:28:18 +0100

    feat: temporary form to add clients to redux store

--- src/features/dashboard/clients/components/ClientForm.tsx
+++ src/features/dashboard/clients/components/ClientForm.tsx
@@ -2,44 +2,65 @@ import { useCallback, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Client } from "../client";
 import { createClient } from "../clientSlice";
-
+const { v1: uuidv1 } = require("uuid");
 function ClientForm() {
+	const dispatch = useDispatch();
 	const [client, setClient] = useState({
+		id: "",
 		name: "",
+		surname: "",
+		phone: "",
+		email: "",
+		summary: "",
 		show: false,
 	});
 
 	const handleChange = (e: any) => {
 		setClient({
 			...client,
-			[e.target.name]: e.target.value,
+			[e.target.name]:
+				e.target.type == "checkbox" ? e.target.checked : e.target.value,
 		});
 		console.log(e.target.name, e.target.value);
 	};
 
 	const handleSubmit = (e: any) => {
 		e.preventDefault();
-	console.log("OLAA")
-	}
+		dispatch(createClient({ ...client, id: uuidv1() }));
+	};
 	return (
-		<form onSubmit={handleSubmit}>
-			<input type="text" placeholder="name" onChange={handleChange} />
+		<form onSubmit={handleSubmit} className="mt-5">
 			<input
-				type="radio"
-				id="html"
-				name="show"
-				value="true"
+				type="text"
+				name="name"
+				placeholder="name"
 				onChange={handleChange}
 			/>
-			<label htmlFor="html">yes</label>
 			<input
-				type="radio"
-				id="css"
-				name="show"
-				value="false"
+				type="text"
+				name="surname"
+				placeholder="surname"
 				onChange={handleChange}
 			/>
-			<label htmlFor="css">no</label>
+			<input
+				type="text"
+				name="phone"
+				placeholder="phone"
+				onChange={handleChange}
+			/>
+			<input
+				type="text"
+				name="email"
+				placeholder="email"
+				onChange={handleChange}
+			/>
+			<input
+				type="text"
+				name="summary"
+				placeholder="summary"
+				onChange={handleChange}
+			/>
+			<input type="checkbox" name="show" id="show" onChange={handleChange} />
 			<button>Save</button>
 		</form>
 	);
