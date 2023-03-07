0c16ddae079c0feba6f666b55f0ffd859590d615
Fran Perea <franciscopereatora@gmail.com>
2023-03-07 13:54:02 +0700

    feat: add more routes to the pages data

--- src/routes/pagesData.tsx
+++ src/routes/pagesData.tsx
@@ -1,5 +1,9 @@
 import Dashboard from "../features/dashboard/Dashboard";
 import Clients from "../features/dashboard/clients/components/Clients";
+import Register from "../features/auth/components/Register";
+import Login from "../features/auth/components/Login";
+import Tasks from "../features/dashboard/tasks/Tasks";
+import Plans from "../features/dashboard/plans/Plans";
 
 export interface IPageRoute {
 	key: string;
@@ -28,7 +32,41 @@ const pageData: IPageRoute[] = [
 				component: <Clients />,
 				path: "clients",
 			},
+			{
+				index: false,
+				exact: false,
+				key: "tasks",
+				component: <Tasks />,
+				path: "tasks",
+			},
+			{
+				index: false,
+				exact: false,
+				key: "plans",
+				component: <Plans />,
+				path: "plans",
+			},
 		],
+	},
+	{
+		key: "register",
+		component: <Register />,
+		path: "/register",
+		children: [
+			{
+				index: true,
+				exact: false,
+				key: "step1",
+				component: <Register />,
+				path: "clients",
+			},
+		],
+	},
+	{
+		key: "login",
+		component: <Login />,
+		path: "/login",
+		children: [],
 	},
 ];
 export default pageData;
